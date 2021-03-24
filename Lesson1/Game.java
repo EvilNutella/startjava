bd04588249881f8ded6cee55177182dd9f11cb53
EvilNutella <72977558+EvilNutella@users.noreply.github.com>
2021-03-24 16:10:51 +0300

    правки в игру

--- Lesson1/Game.java
+++ Lesson1/Game.java
@@ -1,19 +1,19 @@
 public class Game {
     public static void main(String[] args) {
-        int numWin = 15;
+        int targetNumber = 15;
         System.out.println("Ты должен угадать число от 1 до 100. Напиши одно наугад: ");
 
-        int num1 = 5;
-        for (int i = 10; i >= 10; i++) {
-            if (num1 > numWin) {
+        int playerNumber = 1;
+        while (true) {
+            if (targetNumber < playerNumber) {
                 System.out.println("Попробуй снизить ожидания");
-            } else if (num1 < numWin) {
+            } else if (targetNumber > playerNumber) {
                 System.out.println("Бери выше");
-            } else {
-                System.out.println("Невероятно, ты угадал");
-            break;
+            } else if (targetNumber == playerNumber) {
+                break;
             }
-            num1 += i;
+            playerNumber++;
         }
+        System.out.println("Невероятно, ты угадал");
     }
 }
