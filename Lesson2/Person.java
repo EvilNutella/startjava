4f86d810576ae215fe7bc0e0525e53fb811bd17b
EvilNutella <72977558+EvilNutella@users.noreply.github.com>
2021-03-25 00:08:53 +0300

    второй урок дз1

---
+++ Lesson2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String sex = "female";
+    String name = "Wally";
+    float height = 1.6;
+    int weight = 50;
+    int age = 20;
+
+    void walk() {
+
+    }
+    void sit() {
+
+    }
+    void run() {
+
+    }
+    void speak() {
+
+    }
+    void learnJava() {
+
+    }
+}
