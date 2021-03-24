c1d7c5ff7b647eee0de16576c75ea36871518e3e
EvilNutella <72977558+EvilNutella@users.noreply.github.com>
2021-03-24 16:16:33 +0300

    доп.правки в игру

--- Lesson1/Game.java
+++ Lesson1/Game.java
@@ -3,16 +3,17 @@ public class Game {
         int targetNumber = 15;
         System.out.println("Ты должен угадать число от 1 до 100. Напиши одно наугад: ");
 
-        int playerNumber = 1;
+        int playerNumber = 20;
         while (true) {
             if (targetNumber < playerNumber) {
                 System.out.println("Попробуй снизить ожидания");
+                playerNumber--;
             } else if (targetNumber > playerNumber) {
                 System.out.println("Бери выше");
+                playerNumber++;
             } else if (targetNumber == playerNumber) {
                 break;
             }
-            playerNumber++;
         }
         System.out.println("Невероятно, ты угадал");
     }
